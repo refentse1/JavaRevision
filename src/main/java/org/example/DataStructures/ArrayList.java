dd92f7b69774f94e024e18b0bf716e210d818162
refentse1 <refentsemakgobadev@gmail.com>
2023-12-21 17:20:11 +0200

    ArrayList basics covered

---
+++ src/main/java/org/example/DataStructures/ArrayList.java
@@ -0,0 +1,35 @@
+package org.example.DataStructures;
+
+public class ArrayList {
+    /**
+     * <h1>ArrayList</h1>
+     * <p>ArrayList is a resizable array, it can be found in the java.util package.</p>
+     * <p><strong>Unlike an array, an ArrayList can be modified, meaning we can add and remove items , without having to create a new  array.</strong></p>
+     * <p>To specify the array type, we the data type equivalent of a wrapper classes i.e Integer for int, Double for double,Character for char</p>
+     * <h3>ArrayList Methods:</h3>
+     * <ul>
+     *     <li>
+     *         <strong>add():</strong>The argument is the value we want to add at the end of the arraylist
+     *     </li>
+     *     <li>
+     *         <strong>get():</strong>This retrieves the element at the specified index. We pass the index of the value we want to retrieve as an argument
+     *     </li>
+     *     <li>
+     *         <strong>set():</strong>This modifies the value of an existing element. The method takes in the index of the element we want to change the value of and the new value we want to set as the second argument.
+     *     </li>
+     *     <li>
+     *         <strong>remove():</strong>This removes an element from the array. We pass the index of the element we would like to remove as an argument.
+     *     </li>
+     *     <li>
+     *         <strong>clear():</strong>This is used to remove all the elements from the array.
+     *     </li>
+     *     <li>
+     *         <strong>size():</strong>This is used to find the number of elements the arraylist has.
+     *     </li>
+     * </ul>
+     * <br>
+     * <br>
+     * <p>We can loop through the ArrayList using the for and foreach loop just like we do with an array.</p>
+     */
+    public ArrayList(){}
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.example.OOP.Student;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student("John","Doe");
