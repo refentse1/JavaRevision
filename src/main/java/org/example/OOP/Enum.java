3cc7ec8b36cb5a25bbc8c494427306d2b651f412
refentse1 <refentsemakgobadev@gmail.com>
2023-12-21 12:59:39 +0200

    Added some noted for Enumeration

---
+++ src/main/java/org/example/OOP/Enum.java
@@ -0,0 +1,19 @@
+package org.example.OOP;
+
+public class Enum {
+    /**
+     * <h1>Enumerations</h1>
+     * <p>Enum is a special type of class, that represents a group of constants/unchangeable variables.</p>
+     * <p><small>
+     *     <strong>To create an enum, the enum keyword can be used to create the enum class.</strong><br>
+     *     THe constants are comma separated.
+     * </small>
+     * </p>
+     *<br>
+     * <p>To give the constants values, we create a constructor that takes in a parameter, <br>
+     *  then add parenthesis to the enum constant with the argument as the desired value for the enum constant
+     * </p>
+     *
+     */
+    Enum(){}
+}
---
+++ src/main/java/org/example/OOP/Status.java
@@ -0,0 +1,10 @@
+package org.example.OOP;
+
+public enum Status {
+    pending(true),
+    approved(false)
+    ;
+
+    Status(boolean b) {
+    }
+}
